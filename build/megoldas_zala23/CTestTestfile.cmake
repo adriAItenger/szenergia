f211c0fea8a01521114e4e4287df3ad291323b3c
Kazda Adrián <146844112+adriAItenger@users.noreply.github.com>
2024-03-28 02:08:37 +0100

    Delete build directory

--- build/megoldas_zala23/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adrian/megoldas_ws/src/megoldas_zala23
-# Build directory: /home/adrian/megoldas_ws/build/megoldas_zala23
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/megoldas_zala23/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/adrian/megoldas_ws/build/megoldas_zala23/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
